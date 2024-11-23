0f7eb7463fdd604129e7c5adaa54fe52d9c27eda
uygareren <komifb90@hotmail.com>
2024-11-23 20:43:32 +0300

    get User by id api is ok

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,11 +1,29 @@
 const User = require("../models/User");
 
-exports.GetUser = async(req, res) => {
-    try {
-        const users = await User.findAll(); 
-        res.status(200).json(users); 
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error fetching users' });
-      }
-}
+exports.GetUser = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    if (users.length === 0) {
+      return res.status(404).json({ message: 'No users found' });
+    }
+    res.status(200).json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error fetching users' });
+  }
+};
+
+exports.GetUserById = async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const user = await User.findByPk(userId);
+    if (!user) {
+      return res.status(404).json({ message: `User with ID ${userId} not found` });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error fetching user' });
+  }
+};
--- route/UserRoutes.js
+++ route/UserRoutes.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const { Joi, validate } = require('express-validation');
+
+const GetUserValidation = {
+    params: Joi.object({
+        userId: Joi.number().required(),
+    }),
+};
+
+
 const router = express.Router();
 
 const UserController = require("../controller/UserController");
 
 router.get("/users", UserController.GetUser);
+router.get("/users/:userId", validate(GetUserValidation), UserController.GetUserById);
 
 module.exports = router;

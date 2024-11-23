7583afd0c145a8f22ace06354da50c72facf1d6b
uygareren <komifb90@hotmail.com>
2024-11-23 21:30:29 +0300

    create book api is ok

--- controller/BookController.js
+++ controller/BookController.js
@@ -11,4 +11,33 @@ exports.GetBooks = async (req, res) => {
       console.error(error);
       res.status(500).json({ message: 'Error fetching books' });
     }
-  };
+};
+
+exports.GetBookById = async (req, res) => {
+    const {bookId} = req.params;
+
+    try {
+      const book = await Book.findByPk(bookId);
+      if (!book) {
+        return res.status(404).json({ message: `Book with ID ${bookId} not found` });
+      }
+      res.status(200).json(book);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error fetching book' });
+    }
+};
+
+exports.PostBook = async(req, res) => {
+    const {name} = req.body; 
+
+    try {
+        const newBook = await Book.create({title: name});
+        
+        res.status(201).json({ message: 'Book created successfully', book: newBook });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error inserting book' });
+    }
+}
--- route/BookRoutes.js
+++ route/BookRoutes.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const { Joi, validate } = require('express-validation');
 
-const GetUserValidation = {
+const GetBookValidation = {
     params: Joi.object({
-        userId: Joi.number().required(),
+        bookId: Joi.number().required(),
     }),
 };
 
-
+const PostBookValidation = {
+    body: Joi.object({
+        name: Joi.string().required(),
+    }),
+};
 
 const router = express.Router();
 
 const BookController = require("../controller/BookController");
 
 router.get("/", BookController.GetBooks);
+router.get("/:bookId", validate(GetBookValidation), BookController.GetBookById);
+router.post("/", validate(PostBookValidation), BookController.PostBook);
 
 module.exports = router;
